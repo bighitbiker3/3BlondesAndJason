0e2c2a735778c19ba4a7b2b04df5f64d71ee81ac
NICOLAAS KOSTER <nicolaaskoster@nicolaas-mbp.local>
2016-07-27 16:50:34 -0400

    some sweet filters

--- browser/js/common/factories/Product.js
+++ browser/js/common/factories/Product.js
@@ -4,17 +4,18 @@ app.factory('Product', function ($http) {
 
 	var ProductFactory = {};
 
-	ProductFactory.url = '/api/products/'
+	ProductFactory.url = '/api/products'
 
-	ProductFactory.getAll = function() {
-		return $http.get(ProductFactory.url)
+	ProductFactory.getAll = function(query) {
+		if(!query) query = '';
+		return $http.get(ProductFactory.url + query)
 		.then(res => {
       return res.data
     });
 	}
 
 	ProductFactory.getOne = function(id) {
-		return $http.get(ProductFactory.url + id)
+		return $http.get(ProductFactory.url + '/' + id)
 		.then(res => {
 
       return res.data;
--- browser/js/product/list/product.list.js
+++ browser/js/product/list/product.list.js
@@ -1,3 +1,45 @@
-app.controller('ProductListCtrl', function ($scope, products) {
+app.controller('ProductListCtrl', function ($scope, products, Product) {
   $scope.products = products;
+  $scope.filterObj = {};
+
+  $scope.addToFilter = function(property, query){
+  	$scope.filterObj[property] = query;
+  	var superQuery = [];
+  	for (var key in $scope.filterObj) {
+  		if($scope.filterObj.hasOwnProperty(key)){
+  			superQuery.push($scope.filterObj[key]);
+  		}
+  	}
+  	superQuery = '?' + superQuery.join('&');
+
+  	Product.getAll(superQuery)
+  	.then(products => {
+  		$scope.products = products;
+  	})
+  }
+
+  $scope.removeFromFilter = function(property){
+  	delete $scope.filterObj[property];
+  	var superQuery = [];
+  	for (var key in $scope.filterObj) {
+  		if($scope.filterObj.hasOwnProperty(key)){
+  			superQuery.push($scope.filterObj[key]);
+  		}
+  	}
+  	superQuery = '?' + superQuery.join('&');
+
+  	Product.getAll(superQuery)
+  	.then(products => {
+  		$scope.products = products;
+  	})
+  }
+
+  $scope.changeFilter = function(property, query, theCheck){
+  	console.log('scope', $scope[theCheck]);
+  	if($scope[theCheck]){
+  		$scope.addToFilter(property, query);
+  	} else {
+  		$scope.removeFromFilter(property);
+  	}
+  }
 });
