439cc632b2515bee5b623a7ae1093901beeaa70f
Jason Wang <jasonwang67@gmail.com>
2016-07-28 14:01:41 -0400

    working order summaries and details

--- browser/js/profile/orders/myorders.state.js
+++ browser/js/profile/orders/myorders.state.js
@@ -1,6 +1,6 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('profile.orders', {
-    url: '/profile/orders',
+    url: '/orders',
     templateUrl: 'js/profile/orders/myorders.html',
     controller: 'ProfileOrdersCtrl',
     resolve: {
