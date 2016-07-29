ba16801a90c1d3ea1c81a89d110de511004f0338
Elliott McNary <elliottmcnary@gmail.com>
2016-07-29 17:42:54 -0400

    admin styling started

--- browser/js/admin/directives/js/nav.admin.js
+++ browser/js/admin/directives/js/nav.admin.js
@@ -1,7 +1,15 @@
-app.directive('adminNav', function(){
+app.directive('adminNav', function(AuthService){
   return {
     restrict: 'E',
+    scope: {},
     templateUrl: 'js/admin/directives/html/nav.admin.html',
-
+    link: function(scope){
+      var setUser = function () {
+          AuthService.getLoggedInUser().then(function (user) {
+              scope.user = user;
+          });
+      };
+      setUser()
+    }
   }
 });
