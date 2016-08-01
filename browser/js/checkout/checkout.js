11bbb08df8b26807f1dba20dd6ccfc436b33f4c4
NICOLAAS KOSTER <nicolaas.koster@gmail.com>
2016-08-01 11:40:18 -0400

    checkout part 1

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -3,32 +3,12 @@ app.controller('CheckoutCtrl', function($scope, $uibModal, $state, AuthService, 
         purchasedItems: cart
     };
 
-    // add error handling
-    AuthService.getLoggedInUser()
-        .then(function(user) {
-            if (user) {
-            	$scope.newOrder.user = user; 
-            	$scope.newOrder.shippingEmail = user.email;
-                $scope.newOrder.streetAddress = user.streetAddress;           
-                $scope.newOrder.city = user.city;
-                $scope.newOrder.state = user.state;
-                $scope.newOrder.zipCode = user.zipCode;
-            }
-        });
+    $scope.setCurrentShipping = function(address){
+        $scope.shipping = address;
+    }
 
-        $scope.checkout = function() {
+    $scope.setCurrentBilling = function(address){
+        $scope.billing = address;
+    }
 
-            // create order 
-            Order.createOrder($scope.newOrder)
-            .then(function() {
-                $uibModal.open({
-                    templateUrl: 'js/checkout/confirmation.html', 
-                    controller: ['$scope', '$uibModalInstance', '$state', function($scope, $uibModalInstance, $state){
-                        $scope.ok = function() {
-                            $uibModalInstance.close();
-                        }
-                    }]
-                })
-            })
-        }
 })
