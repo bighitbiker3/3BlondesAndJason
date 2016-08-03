289de430ad1d3b4943d510dbd217eab78c0e4d79
NICOLAAS KOSTER <nicolaas.koster@gmail.com>
2016-08-03 11:29:28 -0400

    password reset model edit

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -47,21 +47,21 @@ module.exports = db.define('user', {
 
 }, {
     instanceMethods: {
-        sanitize: function () {
+        sanitize: function() {
             return _.omit(this.toJSON(), ['password', 'salt']);
         },
-        correctPassword: function (candidatePassword) {
-          return this.Model.encryptPassword(candidatePassword, this.salt) === this.password;
+        correctPassword: function(candidatePassword) {
+            return this.Model.encryptPassword(candidatePassword, this.salt) === this.password;
         },
-        changePassword: function(password){
-          return this.setDataValue('password', this.Model.encryptPassword(password, this.salt))
+        changePassword: function(password) {
+            return this.setDataValue('password', this.Model.encryptPassword(password, this.salt))
         }
     },
     classMethods: {
-        generateSalt: function () {
+        generateSalt: function() {
             return crypto.randomBytes(16).toString('base64');
         },
-        encryptPassword: function (plainText, salt) {
+        encryptPassword: function(plainText, salt) {
             var hash = crypto.createHash('sha1');
             hash.update(plainText);
             hash.update(salt);
@@ -70,17 +70,15 @@ module.exports = db.define('user', {
         }
     },
     hooks: {
-        beforeValidate: function (user) {
-            if (user.changed('password') && user.isNewRecord) {
+        beforeCreate: function(user) {
+            user.salt = user.Model.generateSalt();
+            user.password = user.Model.encryptPassword(user.password, user.salt);
+        },
+        beforeUpdate: function(user) {
+            if (user.changed('password')) {
                 user.salt = user.Model.generateSalt();
                 user.password = user.Model.encryptPassword(user.password, user.salt);
             }
-        },
-        beforeUpdate: function(user){
-          if (user.changed('password')) {
-              user.salt = user.Model.generateSalt();
-              user.password = user.Model.encryptPassword(user.password, user.salt);
-          }
         }
     }
 });
